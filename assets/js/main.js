af1c6a77e30aa55a7eb9d9e8f968c90bceb2c290
Fred Pedrozo <frederico.pedrozo@gmail.com>
2021-09-16 19:21:13 -0300

    Responsiveness and content
    Made website responsive, added buttons and a bit of placeholder content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
 // fazer função que verifica se estou em live, se sim: embed link da live. Se não: embed link do trailer da live.
+MathJax = {
+    tex: {
+      inlineMath: [['\\(', '\\)']]
+    }
+  };
